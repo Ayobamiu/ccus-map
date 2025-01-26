6e0f11ff6ed387f5116f4ef4ef4342106cc1e4e4
Ayobamiu <ayobamiu@gmail.com>
2025-01-25 23:43:20 -0500

    Mapbox && FA Icons Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,77 @@
+import { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationCrosshairs } from "@fortawesome/free-solid-svg-icons";
+
+/**
+ * Set Michigan Geological Repository For Research and Education;
+ * Michigan Geological Survey as center of the map
+ */
+const INITIAL_CENTER: [number, number] = [-85.65383, 42.27294];
+const INITIAL_ZOOM = 10.12;
 
 function App() {
+  const [center, setCenter] = useState<[number, number]>(INITIAL_CENTER);
+  const [zoom, setZoom] = useState(INITIAL_ZOOM);
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_PK;
+    if (mapContainerRef.current) {
+      mapRef.current = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        center: center,
+        zoom: zoom,
+      });
+    }
+    if (mapRef.current) {
+      mapRef.current.on("move", () => {
+        // get the current center coordinates and zoom level from the map
+        const mapCenter = mapRef.current?.getCenter();
+        const mapZoom = mapRef.current?.getZoom();
+
+        // update state
+        if (mapCenter) {
+          setCenter([mapCenter.lng, mapCenter.lat]);
+        }
+        if (mapZoom) {
+          setZoom(mapZoom);
+        }
+      });
+    }
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove();
+      }
+    };
+  }, []);
+
+  const handleButtonClick = () => {
+    if (mapRef.current) {
+      mapRef.current.flyTo({
+        center: INITIAL_CENTER,
+        zoom: INITIAL_ZOOM,
+      });
+    }
+  };
+
   return (
-    <div>
-      <p>ccus-app</p>
-    </div>
+    <>
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} |
+        Zoom: {zoom.toFixed(2)}
+      </div>
+      <button className="reset-button" onClick={handleButtonClick}>
+        <FontAwesomeIcon icon={faLocationCrosshairs} size="2x" />
+      </button>
+      <div id="map-container" ref={mapContainerRef} />
+    </>
   );
 }
 
